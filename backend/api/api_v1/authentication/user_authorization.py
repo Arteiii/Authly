fd2a078d0d230852d55856c90ec217b82b230870
wavy42 <wavy_uwu@proton.me>
2023-10-31 22:20:06 +0100

    working on stuff

--- backend/api/api_v1/authentication/user_authorization.py
+++ backend/api/api_v1/authentication/user_authorization.py
@@ -1,11 +1,11 @@
-def check_if_allowed():
-    return
+# def check_if_allowed():
+#     return
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
+# def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+#     user = fake_decode_token(token)
+#     return user
 
 
-async def login(password: str):
+# async def login(password: str):
     
