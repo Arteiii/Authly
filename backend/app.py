d1ae13114a8ee27a6e5121a20136e4f35d90e842
wavy42 <wavy_uwu@proton.me>
2023-11-02 23:54:25 +0100

    fixed: Possible binding to all interfaces. (B104)

--- backend/app.py
+++ backend/app.py
@@ -56,4 +56,4 @@ app.include_router(api_main_router, prefix=config.API.API_ROUTE)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="localhost", port=8000)
