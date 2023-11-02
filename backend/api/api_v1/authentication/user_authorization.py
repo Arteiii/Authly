2eb6e29e00d6910a9648434d47fbb4b37298b787
wavy42 <wavy42@users.noreply.github.com>
2023-11-02 20:37:16 +0000

    :art: Format Python code with psf/black

--- backend/api/api_v1/authentication/user_authorization.py
+++ backend/api/api_v1/authentication/user_authorization.py
@@ -25,7 +25,6 @@ class User(BaseModel):
     disabled: Union[bool, None] = None
 
 
-
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
     if not user:
