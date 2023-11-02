5bb1b5a28629667b4dc84f8248f102f9dfdc955f
wavy42 <wavy_uwu@proton.me>
2023-11-02 17:58:30 +0100

    new logger

--- backend/core/db/redis.py
+++ backend/core/db/redis.py
@@ -1,4 +1,6 @@
-import aioredis
+from redis import asyncio as aioredis
+
+from core.log import Logger
 
 
 class AsyncRedisManager:
@@ -14,7 +16,7 @@ class AsyncRedisManager:
                 f"redis://{self.host}:{self.port}/{self.db}"
             )
         except aioredis.RedisError as e:
-            print(f"Error connecting to Redis: {e}")
+            Logger.debug(f"Error connecting to Redis: {e}")
             return e
         return True
 
@@ -23,7 +25,7 @@ class AsyncRedisManager:
             self.redis_client.close()
             await self.redis_client.wait_closed()
         except aioredis.RedisError as e:
-            print(f"Error closing Redis connection: {e}")
+            Logger.debug(f"Error closing Redis connection: {e}")
             return e
         return True
 
@@ -34,7 +36,7 @@ class AsyncRedisManager:
             else:
                 await self.redis_client.set(key, value)
         except aioredis.RedisError as e:
-            print(f"Error setting value in Redis: {e}")
+            Logger.debug(f"Error setting value in Redis: {e}")
             return e
         return True
 
@@ -43,13 +45,13 @@ class AsyncRedisManager:
             result = await self.redis_client.get(key)
             return result
         except aioredis.RedisError as e:
-            print(f"Error getting value from Redis: {e}")
+            Logger.debug(f"Error getting value from Redis: {e}")
             return e
 
     async def delete(self, key):
         try:
             await self.redis_client.delete(key)
         except aioredis.RedisError as e:
-            print(f"Error deleting key from Redis: {e}")
+            Logger.debug(f"Error deleting key from Redis: {e}")
             return e
         return True
--- backend/core/log.py
+++ backend/core/log.py
@@ -1,4 +1,6 @@
 import sys
+import datetime
+import inspect
 
 from colorama import init, Fore
 
@@ -7,8 +9,21 @@ init(autoreset=True)  # Initialize colorama
 
 class Logger:
     @staticmethod
-    def info(msg: str = "Info Log Message"):
-        print(f"{Fore.BLUE}[INFO]    {msg}{Fore.RESET}")
+    def info(*args):
+        if args:
+            print(
+                f"{Fore.BLUE}[INFO]    {args[0]}{Fore.RESET}"
+            )  # Print the first string on the same line
+            if len(args) > 1:  # Check if there are more than one string
+                for i, msg in enumerate(
+                    args[1:], start=1
+                ):  # Print subsequent strings with indentation
+                    print(
+                        f"{Fore.BLUE}{'          ' * (10 if i == 1 else 14)}\
+                            {msg}{Fore.RESET}"
+                    )
+        else:
+            print(f"{Fore.BLUE}[INFO]    Info Log Message{Fore.RESET}")
 
     @staticmethod
     def warning(msg: str = "Warning Log Message"):
@@ -19,13 +34,62 @@ class Logger:
         print(f"{Fore.RED}[ERROR]    {msg}{Fore.RESET}")
 
     @staticmethod
-    def critical(msg: str = "Critical Log Message"):
-        print(f"{Fore.MAGENTA}[CRITICAL]    {msg}{Fore.RESET}")
+    def critical(*args):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        caller_frame = inspect.currentframe().f_back
+        caller_info = inspect.getframeinfo(caller_frame)
+
+        if args:
+            print(
+                f"{Fore.MAGENTA}|> --- [CRITICAL] -------- [\
+{Fore.LIGHTMAGENTA_EX}{current_time}{Fore.RESET}{Fore.MAGENTA}]\
+--- [{Fore.BLACK}{caller_info.filename}{Fore.MAGENTA}] --- [{Fore.BLUE}\
+{caller_info.function}{Fore.MAGENTA}] --- [{Fore.LIGHTYELLOW_EX}\
+{caller_info.lineno}{Fore.MAGENTA}]"
+            )
+
+        for arg in args[0:]:
+            print(f"{Fore.MAGENTA}|{Fore.RESET} " + arg)
+        print(
+            f"{Fore.MAGENTA}|> -----------------------------------------------\
+{Fore.RESET}"
+        )
         sys.exit(1)
 
     @staticmethod
-    def debug(msg: str = "Debug Log Message"):
-        print(f"{Fore.CYAN}[DEBUG]    {msg}{Fore.RESET}")
+    def debug(*args):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        caller_frame = inspect.currentframe().f_back
+        caller_info = inspect.getframeinfo(caller_frame)
+
+        if args:
+            print(
+                f"{Fore.CYAN}|> --- [DEBUG] -------- [{Fore.LIGHTMAGENTA_EX}\
+{current_time}{Fore.RESET}{Fore.CYAN}]\
+--- [{Fore.BLACK}{caller_info.filename}{Fore.CYAN}] --- [{Fore.BLUE}\
+{caller_info.function}{Fore.CYAN}] --- [{Fore.LIGHTYELLOW_EX}\
+{caller_info.lineno}{Fore.CYAN}]"
+            )
+
+        for arg in args[0:]:
+            print(f"{Fore.CYAN}|{Fore.RESET} " + arg)
+        print(
+            f"{Fore.CYAN}| -----------------------------------------------\
+{Fore.RESET}"
+        )
+
+
+# Example usage of the custom_logger function
+Logger.debug(
+    "This is the first line of the log.",
+    "This is the second line of the log.",
+    "This is the third line of the log.",
+)
+Logger.critical(
+    "This is the first line of the log.",
+    "This is the second line of the log.",
+    "This is the third line of the log.",
+)
 
 
 # mongo schema:
